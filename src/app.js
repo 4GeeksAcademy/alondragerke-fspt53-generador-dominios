5174f1651037d3b332cef0c683bc52c9373ffdb0
Alondra Gerke <141412967+alondragerke@users.noreply.github.com>
2024-01-04 11:22:06 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,94 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateDomains() {
+  let pronoun = ["the", "your", "our", "their"];
+  let adjective = [
+    "strange",
+    "great",
+    "unique",
+    "goofy",
+    "bizarre",
+    "hilarious",
+    "whimsical"
+  ];
+  let noun = [
+    "cat",
+    "horses",
+    "cakes",
+    "neighbor",
+    "planet",
+    "internet",
+    "pride",
+    "bride",
+    "spirit",
+    "benefit",
+    "statue",
+    "issue"
+  ];
+  let extensions = [".com", ".de", ".es", ".it", ".eu", ".net"];
+
+  for (let i = 0; i < 60; i++) {
+    let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+    let randomAdjective =
+      adjective[Math.floor(Math.random() * adjective.length)];
+    let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+    let randomExtension =
+      extensions[Math.floor(Math.random() * extensions.length)];
+
+    let domain = randomPronoun + randomAdjective + randomNoun + randomExtension;
+
+    if (
+      randomExtension.charAt(0) === "." &&
+      randomNoun.endsWith(randomExtension.substring(1))
+    ) {
+      let hackDomain =
+        randomPronoun +
+        randomAdjective +
+        randomNoun.slice(0, -randomExtension.length + 1) +
+        randomExtension;
+      console.log({ hackDomain });
+    } else {
+      console.log({ hackDomain: domain });
+    }
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateDomains();
 };
+
+document.getElementById("generateButton").addEventListener("click", function() {
+  generateDomains();
+});
+
+// OPCIÓN PARA INFINITO, MUY SATURANTE
+
+//  for (let i = 0; i < pronoun.length; i++) {
+//   for (let j = 0; j < adjective.length; j++) {
+//     for (let k = 0; k < noun.length; k++) {
+//       for (let l = 0; l < extensions.length; l++) {
+//         let randomPronoun = pronoun[i];
+//         let randomAdjective = adjective[j];
+//         let randomNoun = noun[k];
+//         let randomExtension = extensions[l];
+//
+//         let domain = randomPronoun + randomAdjective + randomNoun + randomExtension;
+//
+//         if (
+//           randomExtension.charAt(0) === "." &&
+//           randomNoun.endsWith(randomExtension.substring(1))
+//         ) {
+//           let hackDomain =
+//             randomPronoun +
+//             randomAdjective +
+//             randomNoun.slice(0, -randomExtension.length + 1) +
+//             randomExtension;
+//           console.log({ hackDomain });
+//         } else {
+//           console.log({ hackDomain: domain });
+//         }
+//       }
+//     }
+//   }
+// }
